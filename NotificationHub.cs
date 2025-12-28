06c1eedbad583cb06f820bd2e811208f95986630
ARULKUMAR <101092540+ARULKUMAR0106@users.noreply.github.com>
2025-12-28 12:27:36 +0530

    Change Task Status to async and enhance updates
    Refactor Task Status method to be asynchronous and improve job status updates.

---
+++ NotificationHub.cs
@@ -0,0 +1,46 @@
+  private readonly IHubContext<TaskNotificationHub,ITaskNotificationHub> _hub;
+
+  public class NotificationHub{
+   public NotificationHub( ITaskNotificationHub> task)
+   {
+
+       _hub = task;
+
+   }
+  public async  Task Status(string TaskID){
+        
+
+
+                    System.Threading.Tasks.Task SpTask = System.Threading.Tasks.Task.Run(async () =>
+                    {   //do some work
+                        await _hub.Clients.User(userID.ToString())
+                                      .JobStatus("JobCompleted", TaskID);
+                    });
+
+                    System.Threading.Tasks.Task progressTask = System.Threading.Tasks.Task.Run(async () =>
+                    {
+                        if (!string.IsNullOrEmpty(ID))
+                        {
+                            await _hub.Clients.User(userID.ToString())
+                                   .Progress("ProgressUpdate", "1 |"+TaskID);
+                            await System.Threading.Tasks.Task.Delay(TimeSpan.FromSeconds(5));
+                            while (!SpTask.IsCompleted)
+                            {
+
+                                string query = "select Progress from " + Metadata.tableName + " where Id= " + ID;
+
+                                var progress = await _dataSqlRaws.dataretrival(query);
+
+                                await _hub.Clients.User(userID.ToString())
+                                    .JobProgress("ProgressUpdate", progress.StringID+" |"+TaskID);
+
+                                await System.Threading.Tasks.Task.Delay(TimeSpan.FromSeconds(3)); // non-blocking
+                            }
+                        }
+                    });
+
+   }
+
+}
+
+  
